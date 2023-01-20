14d0e3899f1fd7e833c3f122785f975b4e229f6d
Euan Marney <2619355m@student.gla.ac.uk>
2023-01-20 18:02:01 +0000

    Chapter 4

--- rango/views.py
+++ rango/views.py
@@ -2,7 +2,15 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Rango says hey there partner! Go to the <a href='http://127.0.0.1:8000/rango/about/'>About</a> page.")
+    # Construct a dictionary to pass to the template engine as its context.
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+
+    # Return a rendered response to send to the client.
+    return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
-    return HttpResponse("Rango says here is the about page. Go to the <a href='http://127.0.0.1:8000/'>Index</a> page.")
+        # Construct a dictionary to pass to the template engine as its context.
+        context_dict = {'boldmessage': 'This tutorial has been put together by Euan Marney'}
+
+        # Return a rendered response to send to the client.
+        return render(request, 'rango/about.html', context=context_dict)
