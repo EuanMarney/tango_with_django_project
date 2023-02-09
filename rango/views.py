b55a2527373fc2a4c1a45cfa0a9e73d3914efb2f
Euan Marney <2619355m@student.gla.ac.uk>
2023-02-09 15:24:33 +0000

    Chapter 8

--- rango/views.py
+++ rango/views.py
@@ -34,11 +34,8 @@ def index(request):
     return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
-        # Construct a dictionary to pass to the template engine as its context.
-        context_dict = {'boldmessage': 'This tutorial has been put together by Euan Marney'}
-
         # Return a rendered response to send to the client.
-        return render(request, 'rango/about.html', context=context_dict)
+        return render(request, 'rango/about.html')
 
 def add_category(request):
     form = CategoryForm()
